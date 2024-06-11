8464ae245bdc169f9274415864f7268d6fc7571e
Grgeous <cagalfintegioivyerika@gmail.com>
2024-06-11 16:45:30 +0800

    Add files via upload

---
+++ QUIZ HTML/js/quiz.js
@@ -0,0 +1,100 @@
+function input1() {
+    document.getElementById('label1').style.color="red";
+    document.getElementById('label3').style.color="green";
+}
+function input2() {
+    document.getElementById('label2').style.color="red";
+    document.getElementById('label3').style.color="green";
+}
+function input3() {
+    document.getElementById('label3').style.color="green";
+}
+
+
+function input4() {
+    document.getElementById('label4').style.color="red";
+    document.getElementById('label5').style.color="green";
+}
+function input5() {
+    document.getElementById('label5').style.color="green";
+}
+function input6() {
+    document.getElementById('label6').style.color="red";
+    document.getElementById('label5').style.color="green";
+} 
+
+
+
+function input7() {
+    document.getElementById('label7').style.color="red";
+    document.getElementById('label9').style.color="green";
+}
+function input8() {
+    document.getElementById('label8').style.color="red";
+    document.getElementById('label9').style.color="green";
+}
+function input9() {
+    document.getElementById('label9').style.color="green";
+}
+
+
+function input10() {
+    document.getElementById('label10').style.color="green";
+}
+function input11() {
+    document.getElementById('label11').style.color="red";
+    document.getElementById('label10').style.color="green";
+}
+function input12() {
+    document.getElementById('label12').style.color="red";
+    document.getElementById('label10').style.color="green";
+}
+
+
+function input13() {
+    document.getElementById('label13').style.color="green";
+}
+function input14() {
+    document.getElementById('label14').style.color="red";
+    document.getElementById('label13').style.color="green";
+}
+function input15() {
+    document.getElementById('label15').style.color="red";
+    document.getElementById('label13').style.color="green";
+}
+
+
+function input16() {
+    document.getElementById('label16').style.color="green";
+}
+function input17() {
+    document.getElementById('label17').style.color="red";
+    document.getElementById('label16').style.color="green";
+}
+function input18() {
+    document.getElementById('label18').style.color="red";
+    document.getElementById('label16').style.color="green";
+}
+
+
+function input19() {
+    document.getElementById('label19').style.color="green";
+}
+function input20() {
+    document.getElementById('label20').style.color="red";
+    document.getElementById('label19').style.color="green";
+}
+function input21() {
+    document.getElementById('label21').style.color="red";
+    document.getElementById('label19').style.color="green";
+}
+
+
+
+
+
+
+
+
+
+
